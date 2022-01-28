cdae1cfd136024a9abafc6098e06749f350500cd
armandoPineda <armandopineda@wposs.com>
2022-01-28 14:09:00 -0500

    IMPLEMENTACION
    *) se rediseña toda la vista principal en el home

--- AdminParcialPics/app/src/main/java/com/example/adminparcialpics/MainActivity.java
+++ AdminParcialPics/app/src/main/java/com/example/adminparcialpics/MainActivity.java
@@ -8,29 +8,29 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button bt,addMaterias;
+    Button addParciales,addMaterias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bt = findViewById(R.id.bt);
-        addMaterias = findViewById(R.id.materias);
+//        addParciales = findViewById(R.id.bt);
+//        addMaterias = findViewById(R.id.materias);
 
-        addMaterias.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this,IngresarMaterias.class);
-                startActivity(i);
-            }
-        });
-        bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent irFoto2 = new Intent(MainActivity.this,SubirParciales.class);
-                startActivity(irFoto2);
-            }
-        });
+//        addMaterias.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent i = new Intent(MainActivity.this,IngresarMaterias.class);
+//                startActivity(i);
+//            }
+//        });
+//        addParciales.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent irFoto2 = new Intent(MainActivity.this,SubirParciales.class);
+//                startActivity(irFoto2);
+//            }
+//        });
 
 
     }
